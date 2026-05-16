ab82fafb7344a76b930cddb022edb9c4a8436e93
jinx-git-001 <227436341+jinx-git-001@users.noreply.github.com>
2026-05-16 11:14:49 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+DATASET_DIR   = 'dataset'
+IMG_SIZE      = 32
+NUM_CLASSES   = 43
+BATCH_SIZE    = 128
+LR_STAGE1     = 1e-3
+LR_STAGE2     = 1e-4
+MULTIPLIER    = 20
+PER_CLASS     = 20_000
+PATIENCE      = 100
